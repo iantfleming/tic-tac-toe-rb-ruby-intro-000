c550df4e7651495d526763e8c7eba25be9d34677
Ian Fleming <iantfleming1992@gmail.com>
2020-10-20 16:42:56 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -131,7 +131,7 @@ def play(board)
   end
 
   if won?(board)
-    winner =   
+    winner = winner(board)  
     puts "Congratulations #{winner(board)}!"
   elsif draw?(board)
     puts "Cat's Game!"
